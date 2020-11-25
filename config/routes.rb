8168b0c021a80fa6e20f4983748c013018e60d33
Donatien D <ddlu.web@icloud.com>
2020-11-25 11:55:17 +0100

    root implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'villas#index'
+  root to: 'villas#index'
   post '/search', to: 'villas#search'
+  get '/search', to: 'villas#search_results'
   resources :villas, only: [:new, :create]
-  resources :villas, only: [:index, :show] do
+  resources :villas, only: [:show] do
     resources :bookings, only: [:new, :create]
     # resources :reviews, only: [:new, :create]
   end
